abbfd249645f60137c420d3fb4106d65de8d2dcb
Néstor Salceda <nestor.salceda@sysdig.com>
2019-08-08 12:33:02 +0200

    Add tests for HTTP endpoint for /resources

--- web/api.go
+++ web/api.go
@@ -1,10 +1,34 @@
 package web
 
 import (
+	"cloud-native-visibility-hub/pkg/resource"
+	"cloud-native-visibility-hub/pkg/usecases"
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
-func retrieveAllResourcesHandler(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+type dummyResourcesRepository struct {
 }
 
+func (resources *dummyResourcesRepository) All() ([]resource.Resource, error) {
+	return []resource.Resource{
+		{
+			Name: "Falco profile for Nginx",
+		},
+		{
+			Name: "Grafana Dashboard for Traefik",
+		},
+	}, nil
+}
+
+func retrieveAllResourcesHandler(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	useCase := usecases.RetrieveAllResources{
+		Resources: &dummyResourcesRepository{},
+	}
+
+	resources, _ := useCase.Execute()
+	resourcesAsJSON, _ := json.Marshal(resources)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(resourcesAsJSON)
+}
--- web/api_test.go
+++ web/api_test.go
@@ -1,13 +1,16 @@
 package web
 
 import (
+	"cloud-native-visibility-hub/pkg/resource"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestRetrieveAllResourcesHandler(t *testing.T) {
+func TestRetrieveAllResourcesHandlerReturnsHTTPOk(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/resources", nil)
 	recorder := httptest.NewRecorder()
 
@@ -15,4 +18,41 @@ func TestRetrieveAllResourcesHandler(t *testing.T) {
 	router.ServeHTTP(recorder, request)
 
 	assert.Equal(t, recorder.Code, http.StatusOK)
+}
+
+func TestRetrieveAllResourcesHandlerReturnsResources(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/resources", nil)
+	recorder := httptest.NewRecorder()
+
+	router := NewRouter()
+	router.ServeHTTP(recorder, request)
+
+	var result []resource.Resource
+	body, _ := ioutil.ReadAll(recorder.Body)
+	json.Unmarshal([]byte(body), &result)
+
+	assert.Equal(t, result, []resource.Resource{
+		{
+			Name: "Falco profile for Nginx",
+		},
+		{
+			Name: "Grafana Dashboard for Traefik",
+		},
+	})
+}
+
+func TestRetrieveAllResourcesHandlerReturnsAJSONResponse(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/resources", nil)
+	recorder := httptest.NewRecorder()
+
+	router := NewRouter()
+	router.ServeHTTP(recorder, request)
+
+	assert.Equal(t, recorder.HeaderMap, jsonHeader())
+}
+
+func jsonHeader() http.Header {
+	jsonHeader := http.Header{}
+	jsonHeader.Set("Content-Type", "application/json")
+	return jsonHeader
 }
