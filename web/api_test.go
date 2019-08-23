eebfe6f4486db65c567388baf74c190f8e202d7e
Federico Barcelona <fede_rico_94@hotmail.com>
2019-08-23 13:35:16 +0200

    Remove unneeded parameter from tests

--- web/api_test.go
+++ web/api_test.go
@@ -12,23 +12,23 @@ import (
 )
 
 func TestRetrieveAllResourcesHandlerReturnsHTTPOk(t *testing.T) {
-	testRetrieveAllReturnsHTTPOk(t, "/resources", "../test/fixtures/resources")
+	testRetrieveAllReturnsHTTPOk(t, "/resources")
 }
 
 func TestRetrieveOneResourceHandlerReturnsHTTPOk(t *testing.T) {
 	apacheHash := "ijdncmo23ix4j5cgdwwh"
-	testRetrieveAllReturnsHTTPOk(t, "/resources/"+apacheHash, "../test/fixtures/resources")
+	testRetrieveAllReturnsHTTPOk(t, "/resources/"+apacheHash)
 }
 
 func TestRetrieveAllVendorsHandlerReturnsHTTPOk(t *testing.T) {
-	testRetrieveAllReturnsHTTPOk(t, "/vendors", "../test/fixtures/vendors")
+	testRetrieveAllReturnsHTTPOk(t, "/vendors")
 }
 
 func TestRetrieveOneVendorsHandlerReturnsHTTPOk(t *testing.T) {
-	testRetrieveAllReturnsHTTPOk(t, "/vendors/apache", "../test/fixtures/vendors")
+	testRetrieveAllReturnsHTTPOk(t, "/vendors/apache")
 }
 
-func testRetrieveAllReturnsHTTPOk(t *testing.T, path, fixturesPath string) {
+func testRetrieveAllReturnsHTTPOk(t *testing.T, path string) {
 	request, _ := http.NewRequest("GET", path, nil)
 	recorder := httptest.NewRecorder()
 	os.Setenv("RESOURCES_PATH", "../test/fixtures/resources")
