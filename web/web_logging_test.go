cd0c61ea56f9deacd7dc8ad69ef405cba11e1fbe
Néstor Salceda <nestor.salceda@sysdig.com>
2020-01-03 17:33:01 +0100

    Wire API and usecases with new slugs
    Signed-off-by: Néstor Salceda <nestor.salceda@sysdig.com>

--- web/web_logging_test.go
+++ web/web_logging_test.go
@@ -14,13 +14,13 @@ import (
 
 var _ = Describe("HTTP Server Logging", func() {
 	It("is logging requests", func() {
-		request, _ := http.NewRequest("GET", "/resources/apache/custom-rules.yaml", nil)
+		request, _ := http.NewRequest("GET", "/resources/falco-rules/apache/custom-rules.yaml", nil)
 		recorder := httptest.NewRecorder()
 
 		buff := &bytes.Buffer{}
 		router := web.NewRouterWithLogger(log.New(buff, "", 0))
 		router.ServeHTTP(recorder, request)
 
-		Expect("200 [] GET /resources/apache/custom-rules.yaml\n").To(Equal(buff.String()))
+		Expect("200 [] GET /resources/falco-rules/apache/custom-rules.yaml\n").To(Equal(buff.String()))
 	})
 })
